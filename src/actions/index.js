a4787e4c9a1786a66d7f077e2eeb16bcefd1f683
Jose Villarroel <oscarclarke9900@gmail.com>
2019-01-22 01:15:54 -0400

    memoized in order to slender the amount of API fetch for the users.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
 
 export const fetchPosts = () => async (dispatch) => {
     try {
@@ -10,12 +11,34 @@ export const fetchPosts = () => async (dispatch) => {
     }
 }
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => {
+    _fetchUser(id, dispatch);
+}
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
     try {
-        const response = await jsonPlaceholder.get(`./users/${id}`);
+        const response = await jsonPlaceholder.get(`/users/${id}`);
 
         dispatch({ type: 'FETCH_USER', payload: response.data })
     } catch (e) {
         console.log('Error while fetching data for user')
     }
+}); 
+
+//ES5 version besides Async await
+/*export var fetchUser = function(id) {
+    return function(dispatch) {
+        _fetchUser(id, dispatch)
+    }
 }
+
+var _fetchUser = _.memoize(async function(id, dispatch){
+    try {
+        var response = await jsonPlaceholder.get('/users/' + id);
+
+        dispatch({ type: 'FETCH_USER', payload: response.data })
+    } catch (e) {
+        console.log('Error while fetching data for user')
+    }
+})
+*/
